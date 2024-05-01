22dc3a643bad8b100bae55810ef5198d356f280b
Arnit Sinha <75295458+arnitsinha@users.noreply.github.com>
2024-05-01 13:59:52 -0400

    Time: 26 ms (99.33%) | Memory: 13.5 MB (6.81%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,8 +6,19 @@ class Solution(object):
         :rtype: List[int]
         """
         num_indices = {}
+        # Populate the dictionary with indices of elements
+        for i, num in enumerate(nums):
+            if num not in num_indices:
+                num_indices[num] = []
+            num_indices[num].append(i)
+        
+        # Iterate through the array to find the pair
         for i, num in enumerate(nums):
             complement = target - num
             if complement in num_indices:
-                return [num_indices[complement], i]
-            num_indices[num] = i
+                # If the complement exists and it's not the same as num
+                if complement != num:
+                    return [i, num_indices[complement][0]]
+                # If the complement is the same as num, check if there are more than one occurrences
+                elif len(num_indices[complement]) > 1:
+                    return [i, num_indices[complement][1]]
