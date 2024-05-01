ad03472854c3a44261b2e5380dac36ee1807fa6a
Arnit Sinha <75295458+arnitsinha@users.noreply.github.com>
2024-05-01 14:00:40 -0400

    Time: 34 ms (89.66%) | Memory: 12.8 MB (6.81%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,24 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        num_indices = {}
-        # Populate the dictionary with indices of elements
-        for i, num in enumerate(nums):
-            if num not in num_indices:
-                num_indices[num] = []
-            num_indices[num].append(i)
-        
-        # Iterate through the array to find the pair
+        seen = {}
         for i, num in enumerate(nums):
             complement = target - num
-            if complement in num_indices:
-                # If the complement exists and it's not the same as num
-                if complement != num:
-                    return [i, num_indices[complement][0]]
-                # If the complement is the same as num, check if there are more than one occurrences
-                elif len(num_indices[complement]) > 1:
-                    return [i, num_indices[complement][1]]
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
