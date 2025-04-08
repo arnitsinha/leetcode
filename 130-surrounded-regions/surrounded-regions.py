0eb8d6e133e648b87217e2a7112e21ee24d769ef
Arnit <75295458+arnitsinha@users.noreply.github.com>
2025-04-09 00:17:32 +0800

    Time: 9 ms (62.06%) | Memory: 17 MB (30.25%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        if not board:
+            return
+        
+        rows, cols = len(board), len(board[0])
+        
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != 'O':
+                return
+            board[r][c] = 'T'  # Temporary mark
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        
+        # Step 1: Mark all border 'O's and their connected regions as 'T'
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O' and (r == 0 or r == rows - 1 or c == 0 or c == cols - 1):
+                    dfs(r, c)
+        
+        # Step 2: Flip all remaining 'O's to 'X' and 'T's back to 'O's
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'T':
+                    board[r][c] = 'O'
